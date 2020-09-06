c57fc3e2b593bad05dd6d0053d0820522c5b2c13
RustamElkin <ryolkin90@gmail.com>
2020-09-06 23:47:42 +0600

    update feed_screen and photo_screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 // import '../lib/screens/feed_screen.dart';
-// // import '../lib/screens/photo_screen.dart';
+// import '../lib/screens/photo_screen.dart';
 // import '../lib/widgets/widgets.dart';
 
 // void main() {
