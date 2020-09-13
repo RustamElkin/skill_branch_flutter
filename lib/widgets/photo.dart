ce6477685433ecc7905c2de98759f91f1af49295
RustamElkin <ryolkin90@gmail.com>
2020-09-13 22:37:18 +0600

    fix

--- lib/widgets/photo.dart
+++ lib/widgets/photo.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:FlutterGalleryApp/res/res.dart';
 
 class Photo extends StatelessWidget {
