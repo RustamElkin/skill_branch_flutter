bc934b771ebfb7f0570491e21a4a71e7aafd02ed
RustamElkin <ryolkin90@gmail.com>
2020-09-13 23:56:36 +0600

    fix

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -32,7 +32,6 @@ class _FullScreenImageState extends State<FullScreenImage> {
     altDescription =
         (widget.altDescription != null) ? widget.altDescription : "";
     photo = (widget.photo != null) ? widget.photo : kFlutterDash;
-    // "https://flutter.dev/assets/404/dash_nest-c64796b59b65042a2b40fae5764c13b7477a592db79eaf04c86298dcb75b78ea.png";
     userName = (widget.userName == null) ? 'kaparray' : widget.userName;
     name = (widget.name != null) ? widget.name : "Kirill Adeshchenko";
   }
